4fa9916367e42b2fd71bfb3717d7a9e3550ef4a0
T-mann6 <113894304+T-mann6@users.noreply.github.com>
2023-01-09 13:20:55 +0100

    added stuff

---
+++ operator.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Mitt første PHP-dokument</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+        $duhqoiwuehdrpqwoieuråoqi1 = 2390234023492;
+        $oiwefjweofjpwoejfpowejirfopw2 = 45786;
+        $skrive1 = "Håkon";
+        $skrive2 = "Kløkstad";
+
+        $plus = $duhqoiwuehdrpqwoieuråoqi1 + $oiwefjweofjpwoejfpowejirfopw2;
+        $minus = $duhqoiwuehdrpqwoieuråoqi1 - $oiwefjweofjpwoejfpowejirfopw2;
+        $gange = $duhqoiwuehdrpqwoieuråoqi1 * $oiwefjweofjpwoejfpowejirfopw2;
+        $deling = $duhqoiwuehdrpqwoieuråoqi1 / $oiwefjweofjpwoejfpowejirfopw2;
+
+        $tekst1 = $skrive1 . $skrive2;
+        $tekst2 = $skrive1 . " " . $skrive2 . "s vei";
+
+        echo "Summen blir $plus <br>";
+        echo "Differansen blir $minus <br>";
+        echo "Produktet blir $gange <br>";
+        echo "Kvotienten blir $deling <br>";
+
+        echo "<br><br>";
+
+        echo "$tekst1 <br>";
+        echo $tekst2;
+        echo "<br><br>";
+
+        echo "<a href='https://nfk.itslearning.com/index.aspx'>itslearning</a>";
+        ?>
+    </body>
+</html>
